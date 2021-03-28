417fb1f4a43f95e583adfd9981859f8b29658ad6
ZUBAIR AHMED <saad03261@gmail.com>
2021-03-28 13:56:13 +0600

    Lab-3 added

---
+++ Final Term Lab-3/Test Interface 3/Test Interface 3/MusicFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_3
+{
+    class MusicFile
+    {
+        private string title;
+        private string artist;
+        private int yearOfRelease;
+        private int durationInSeconds;
+
+        public MusicFile()
+        { }
+        public MusicFile(string title, string artist, int yearOfRelease, int durationInSeconds)
+        {
+            this.title = title;
+            this.artist = artist;
+            this.yearOfRelease = yearOfRelease;
+            this.durationInSeconds = durationInSeconds;
+
+        }
+
+
+
+    }
+}
---
+++ Final Term Lab-3/Test Interface 3/Test Interface 3/RadioPlayerInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_3
+{
+    interface RadioPlayerInterface
+    {
+        void Switch(bool on);
+        void retune(double frequency);
+
+        void setVolume(int loudness);
+        void changeChannel();
+
+    }
+
+}
